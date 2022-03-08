f55e8e640a4e6c4484bd2f4546324abd66caa897
Garrett Armstrong <armstrongg@uncw.edu>
2022-03-08 13:32:33 -0500

    panel switching starting to work

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
 })
 
+router.get('/trash', function (req, res, next) {
+  res.render('trash')
+})
+
 module.exports = router

406523ebdeeccf15b94e370930ccf9a5ddd2bf7e
Garrett Armstrong <armstrongg@uncw.edu>
2022-03-11 18:02:03 -0500

    on login, edit buttons appear

--- app/ldap.js
+++ app/ldap.js
@@ -1,27 +1,12 @@
-let ldap
-
-// from version currently running on rancher sierra-reports
-if (process.env.NODE_ENV === 'production') {
-  ldap = {
-    server: {
-      url: 'ldaps://ldaps.uncw.edu:636',
-      bindDN: process.env.LDAP_USER,
-      bindCredentials: process.env.LDAP_PASS,
-      searchBase: 'dc=uncw,dc=edu',
-      searchFilter: '(&(sAMAccountName={{username}})(memberOf=CN=Library,OU=LIB,OU=AA,OU=Faculty-Staff,DC=uncw,DC=edu))'
-    }
-  }
-} else {
-// from working dev box barcode-lookup
-  ldap = {
-    server: {
-      url: 'ldap://ldap.uncw.edu:389',
-      bindDN: process.env.LDAP_USER,
-      bindCredentials: process.env.LDAP_PASS,
-      searchBase: 'dc=uncw,dc=edu',
-      searchFilter: '(&(sAMAccountName={{username}})(memberOf=CN=Library,OU=LIB,OU=AA,OU=Faculty-Staff,DC=uncw,DC=edu))'
-    }
+ldap = {
+  server: {
+    url: `${process.env.LDAP_URL}:${process.env.LDAP_PORT}`,
+    bindDN: process.env.LDAP_USER,
+    bindCredentials: process.env.LDAP_PASS,
+    searchBase: 'dc=uncw,dc=edu',
+    searchFilter: '(&(sAMAccountName={{username}})(memberOf=CN=Library,OU=LIB,OU=AA,OU=Faculty-Staff,DC=uncw,DC=edu))'
   }
 }
+
 
 module.exports = ldap
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const passport = require('passport')
+
 const router = express.Router()
+
 const plotController = require('../controllers/plot.js')
 const searchController = require('../controllers/search.js')
 const actionAreasController = require('../controllers/actionAreas.js')
 
 
 router.get('/', async function (req, res, next) {
-  // plotData is sent to client js, so it must be json encoded.
+
+  // plot data is sent to client js, so it must be json encoded.
   const plot = JSON.stringify(await plotController.getPlotData())
   const searchOptions = await searchController.getSearchOptions()
   const actionAreas = await actionAreasController.getActionAreas()
@@ -15,10 +19,29 @@ router.get('/', async function (req, res, next) {
     title: 'Actual App',
     plotData: plot,
     searchOptionsData: searchOptions,
-    actionAreasData: actionAreas
+    actionAreasData: actionAreas,
+    loggedIn: req.isAuthenticated()
   }
   res.render('index.hbs', payload)
 })
+
+router.get('/login', function (req, res, next) {
+  res.render('login', {
+    layout: 'layout',
+    failure: (req.query.failure),
+    loggedIn: false
+  })
+})
+
+router.post('/login', passport.authenticate('ldapauth', { failureRedirect: '/login?failure=true' }), async (req, res, next) => {
+  res.redirect('/')
+})
+
+router.get('/logout', function (req, res, next) {
+  req.logout()
+  res.redirect('/')
+})
+
 
 
 router.get('/reference', function (req, res, next) {
